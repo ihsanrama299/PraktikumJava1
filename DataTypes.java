5616688cc5de6ecb1904e0da9d3a73dec983f7aa
ihsanrama299 <ihsanramadhan299@gmail.com>
2021-05-29 15:25:53 +0800

    Output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -9,5 +9,15 @@ public class DataTypes {
 		double ipk = 3.89764512;
 		char nilaiAbjad = 'A';
 		boolean statusAktif = true;
+		
+		System.out.println("Nama depan : " + namaDepan);
+		System.out.println("Nama tengah : " + namaTengah);		
+		System.out.println("Nama belakang : " + namaBelakang);
+		System.out.println("Usia : " + usia);
+		System.out.println("Target kuliah : " + targetKuliah + " tahun");
+		System.out.println("IPK : " + ipk);
+		System.out.println("Nama PBO : " + nilaiAbjad);
+		System.out.println("Status kuliah aktif : " + statusAktif);
+
 	}
 }
