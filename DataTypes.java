763833c49668a4c07e93d3f8c5f000f9d458b5c6
ihsanrama299 <ihsanramadhan299@gmail.com>
2021-05-29 15:19:48 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+	public static void main (String[] args) {
+		
+		String namaDepan = "Muhammad";
+		String namaTengah = "Ihsan";
+		String namaBelakang = "Ramadhan";
+		int usia = 20;
+		int targetKuliah = 4;
+		double ipk = 3.89764512;
+		char nilaiAbjad = 'A';
+		boolean statusAktif = true;
+	}
+}
